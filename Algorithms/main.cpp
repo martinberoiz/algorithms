7b3344fd6f2aa88b50b513592d0417a16923f8f9
Martin Beroiz <martinberoiz@phys.utb.edu>
2014-02-28 10:44:26 -0600

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Martin Beroiz on 2/28/14.
+//  Copyright (c) 2014 CGWA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
