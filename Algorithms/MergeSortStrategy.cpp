1dff8fe0d680fb4a99607e7660a0d78a7e09d658
Martin Beroiz <martin@CGWAs-MacBook-Pro-I.local>
2014-04-05 19:28:09 -0500

    Made a stable (the previous one was stable too) version of merge sort, following the implementation given in Algorithms course in Coursera.

--- Algorithms/MergeSortStrategy.cpp
+++ Algorithms/MergeSortStrategy.cpp
@@ -33,11 +33,18 @@ template <class T> void MergeSortStrategy<T>::topDownSort(T* array, int len) {
 template <class T> void MergeSortStrategy<T>::merge(T* arrayPointer, int len1, int len2) {
     
     memcpy(workArray, arrayPointer, (len1 + len2) * sizeof(T));
+    
     T* subArray1 = workArray;
     T* subArray2 = workArray + len1;
     int i = 0, j = 0, ind = 0;
-    while (i < len1 && j < len2) {
-        if (subArray1[i] > subArray2[j]) {
+    while (ind < len1 + len2) {
+        if (i == len1) {
+            arrayPointer[ind] = subArray2[j]; //This is actually not necessary
+            j++;
+        } else if(j == len2) {
+            arrayPointer[ind] = subArray1[i];
+            i++;
+        } else if (subArray1[i] > subArray2[j]) {
             arrayPointer[ind] = subArray2[j];
             j++;
         } else {
@@ -47,43 +54,5 @@ template <class T> void MergeSortStrategy<T>::merge(T* arrayPointer, int len1, i
         ind++;
     }
     
-    //If the loop exited with the second array exhausted, put the remaining elements
-    //of the first subarray at the end of the arrayPointer. (This is not necessary if the first
-    //subarray is exhausted first, since the elements of the 2nd subarray are already copied in arrayPointer.
-    if (j == len2) {
-        for (int k = i; k < len1; k++) {
-            arrayPointer[ind++] = subArray1[k];
-        }
-    }
 }
-
-/*template <class T> void MergeSortStrategy<T>::merge_old(T* arrayPointer, int len1, int len2) {
-    
-    workVector->clear();
-    T* subArray1 = arrayPointer;
-    T* subArray2 = arrayPointer + len1;
-    int i = 0, j = 0;
-    while (i < len1 && j < len2) {
-        if (subArray1[i] > subArray2[j]) {
-            workVector->push_back(subArray2[j]);
-            j++;
-        } else {
-            workVector->push_back(subArray1[i]);
-            i++;
-        }
-    }
-    if (i == len1) {
-        for (int k = j; k < len2; k++) {
-            workVector->push_back(subArray2[k]);
-        }
-    }
-    if (j == len2) {
-        for (int k = i; k < len1; k++) {
-            workVector->push_back(subArray1[k]);
-        }
-    }
-    for (int i = 0; i < workVector->size(); i++) arrayPointer[i] = (*workVector)[i];
-    
-}*/
-
 
